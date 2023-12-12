bc245108316c4ea0b04f073190055e00c1a0d90a
Aaron <arnhsmith@gmail.com>
2023-12-12 11:15:22 -0500

    added contains-duplicates.js

---
+++ arrays-and-hashing/contains-duplicates.js
@@ -0,0 +1,25 @@
+var containsDuplicate = function(nums) {
+    
+    // creates a new set that contains only unique values when added
+    const set = new Set();
+
+    // checks if the set contains duplicate values, if not it will add them to the set
+
+    // loop through the array of nums using the 
+    for (let num of nums) {
+        // checks num against set, if set contains a duplicate value then it will return true
+        if (set.has(num)) {
+            return true;
+        }
+        // if number is unique then it will add it to the set to be checked later
+        set.add(num);
+        console.log(set)
+    }
+
+    // returns false if array has no duplicate values
+    return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]))
+console.log(containsDuplicate([1, 2, 3, 4]))
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
