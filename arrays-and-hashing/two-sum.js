9958b64746256c7809f8fd0ab091ef17b325c689
Aaron <arnhsmith@gmail.com>
2023-12-12 11:57:22 -0500

    added two-sum.js

---
+++ arrays-and-hashing/two-sum.js
@@ -0,0 +1,27 @@
+var twoSum = function(nums, target) {
+  // to solve this we should create a map which will create key value pairs that we subtract from the target and if there is a match then we will return the index of the values that add up to the target
+
+  // define new map object
+  const numsMap = new Map();
+
+  // loop through the values and if not found we will add to the map to compare to the target - number later
+  for (let i = 0; i < nums.length; i++) {
+
+    // set a difference between the target and the current number
+    const difference = target - nums[i];
+
+    // checks if the map object has the difference value and returns it
+    if (numsMap.has(difference)) {
+      console.log([numsMap.get(difference), i]);
+      return [numsMap.get(difference), i];
+    }
+    // if it doesn't have the difference it will add to the map object to compare it later
+    numsMap.set(nums[i], i);
+  }
+  // return empty array if no two number add up to the target number
+  return [];
+};
+
+twoSum([2, 7, 11, 15], 9);
+twoSum([3, 2, 4], 6);
+twoSum([3, 3], 6);
