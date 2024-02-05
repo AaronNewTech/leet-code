ba7916777a8640bce246b5ab4179618cd4f61960
Aaron Smith <arnhsmith@gmail.com>
2024-02-05 18:40:37 -0500

    added big-countries.sql

---
+++ sql/big-countries.sql
@@ -0,0 +1,7 @@
+-- to get multiple keys in the result use commas like this
+SELECT name, population, area
+
+FROM world
+
+-- use greater than and equal syntax these values
+WHERE area >= '3000000' OR population >= '25000000'
--- sql/find-customer-referee.sql
+++ sql/find-customer-referee.sql
@@ -1,8 +1,11 @@
 SELECT name
+
 FROM customer
+
+-- to get all the customers wtihout a referee id of 2 this is the syntax
 WHERE referee_id IS NULL OR referee_id != 2
 
--- another syntax
+-- another syntax to get the solution
 -- select name 
 -- from Customer 
 -- where referee_id<>2 or referee_id is NULL;
