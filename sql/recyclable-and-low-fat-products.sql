245cff78a871d1828a9317bd9204912637964ebb
Aaron Smith <arnhsmith@gmail.com>
2024-02-05 17:14:44 -0500

    added recyclable-and-low-fat-products.sql

---
+++ sql/recyclable-and-low-fat-products.sql
@@ -0,0 +1,8 @@
+-- this is output that I want which is the product_id
+SELECT product_id
+
+-- this is the table which is products
+FROM products
+
+-- this is the conditions which are low fat and recyclable
+WHERE low_fats = 'Y' AND recyclable = 'Y'
