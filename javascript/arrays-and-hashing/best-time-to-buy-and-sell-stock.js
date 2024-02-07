c4d19bbe12045622b446ebf6bf979abb61b9379e
Aaron Smith <arnhsmith@gmail.com>
2024-02-07 12:37:56 -0500

    updated best-time-to-buy-and-sell-stock.js

--- javascript/arrays-and-hashing/best-time-to-buy-and-sell-stock.js
+++ javascript/arrays-and-hashing/best-time-to-buy-and-sell-stock.js
@@ -1,11 +1,24 @@
 var maxProfit = function(prices) {
+    // problem in my own words
+    // find to best day to buy and sell stock and return the maximum profit possible
+
+    // instructions
+    // use Math javascript functions to find the the lowest and highest numbers in the array
+
+    // initialize profit and minimum price variables
     let profit = 0
     let min = prices[0]
 
+    // loop for interating through prices array to find min price
     for (let i = 1; i < prices.length; i++) {
+
+        // updates the min price if a lower one is found
         min = Math.min(min, prices[i])
+
+        // updates the profit if current price minus min is greater than existing profit
         profit = Math.max(profit, prices[i] - min)
     }
+
     return profit
 };
 
